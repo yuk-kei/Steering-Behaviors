e50b484d88d17bd4b6d2e8b9c0130a2c9e08dd4f
Yukkei Ho <yukkei_mh@outlook.com>
2021-11-14 07:33:08 +0800

    flee behavior

---
+++ SBF-Flee/WebContent/main.js
@@ -0,0 +1,93 @@
+var WIDTH = 1000;
+var HEIGHT = 600;
+var LOGIC_FRAME = 30;
+
+var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+
+var boidsAmount = 1;
+var boids = [];
+var target;
+var frameCounter = 0;
+var fleeCounter = 0;
+
+function preload() {
+
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+
+    game.load.image("boid", "assets/sprites/ship.png");
+    game.load.image("target", "assets/sprites/circle.png");  
+
+}
+
+function create() {
+    game.stage.backgroundColor = '#000000';
+    target = game.add.sprite(game.width / 2, game.height / 2, "target");
+    //The anchor sets the origin point of the texture.
+    // Setting than anchor to 0.5 means the textures origin is centered
+    target.anchor.set(0.5);
+
+    initBoids();
+}
+
+function update() {
+
+    for (var i = 0; i < boidsAmount; i++) {
+        flee(boids[i]);
+        boids[i].body.onWorldBounds = new Phaser.Signal();
+        boids[i].body.onWorldBounds.add(resetTarget,this);
+    }
+}
+
+function initBoids() {
+    for (var i = 0; i < boidsAmount; i++) {
+        var randomPoint = new Phaser.Point(game.rnd.between(target.x - 30, target.x + 30), game.rnd.between(target.y -30, target.y + 30));
+        boids[i] = game.add.sprite(randomPoint.x, randomPoint.y, "boid")
+        boids[i].anchor.set(0.5);
+        boids[i].speed = game.rnd.between(100,200);
+        boids[i].force = game.rnd.between(10, 20);
+        game.physics.enable(boids[i], Phaser.Physics.ARCADE);
+        boids[i].body.collideWorldBounds = true;
+        boids[i].body.bounce.setTo(1,1);
+        boids[i].body.allowRotation = false;
+    }
+}
+
+function flee(boid) {
+    var direction = new Phaser.Point(boid.x, boid.y);
+    direction.subtract(target.x, target.y);
+    direction.normalize();
+    direction.setMagnitude(boid.speed);
+    direction.subtract(boid.body.velocity.x, boid.body.velocity.y);
+    direction.normalize();
+    direction.setMagnitude(boid.force);
+    boid.body.velocity.add(direction.x, direction.y);
+
+    boid.body.velocity.normalize();
+    boid.body.velocity.setMagnitude(boid.speed);
+
+    boid.angle = 180 + Phaser.Math.radToDeg(Phaser.Point.angle(boid.position, new Phaser.Point(boid.x + boid.body.velocity.x, boid.y + boid.body.velocity.y)));
+
+}
+
+function resetTarget(boid) {
+
+        target.x = game.rnd.between(10, game.width - 10);
+        target.y = game.rnd.between(10, game.height - 10);
+        boid.x = game.rnd.between(target.x - 100, target.x + 100);
+        boid.y = game.rnd.between(target.y - 100, target.y + 100);
+        fleeCounter++;
+}
+
+function render() {
+    game.debug.text("Flee times :"+ fleeCounter, 10, 20);
+}
+
+// direction vector is the straight direction from the target to the boid
+// subtract the current boid position
+// then normalize it. A normalized vector has its length is 1, but it retains the same direction
+// time to set magnitude (length) to boid speed
+// then subtract the current boid velocity
+// normalizing again
+// set the magnitude to boid force, which should be WAY lower than its velocity
+// we normalize the velocity and set the magnitude to boid speed
